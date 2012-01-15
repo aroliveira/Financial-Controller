cff6e5ae1fbdc596ed7d8c2ea5b182a02c656c52
aroliveira <aroliveira.info@gmail.com>
2012-01-15 15:17:49 -0200

    Inclusão do form de cadastro de campo. Commit parcial

---
+++ financial-controller-war/src/main/java/aroliveira/converter/driver/TypeConverter.java
@@ -0,0 +1,26 @@
+package aroliveira.converter.driver;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+import aroliveira.lab.structure.util.Type;
+
+@FacesConverter(forClass=aroliveira.lab.structure.util.Type.class)
+public class TypeConverter implements Converter {
+
+	@Override
+	public Object getAsObject(FacesContext context, UIComponent component,
+			String textType) {
+		
+		return Type.valueOf(textType);
+	}
+
+	@Override
+	public String getAsString(FacesContext context, UIComponent component,
+			Object type) {
+		
+		return type.toString();
+	}
+}
